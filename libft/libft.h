f40c06130a983e0ccf4285ef51bd9a56980f10af
Tyssan <tbrunier@student.42perpignan.fr>
2024-01-03 16:22:04 +0100

    bzero

--- libft/libft.h
+++ libft/libft.h
@@ -26,6 +26,7 @@ typedef struct s_list
 }					t_list;
 
 int			ft_atoi(const char *nptr);
+void		ft_bzero(void *s, size_t n);
 int			ft_isalnum(int c);
 int			ft_isalpha(int c);
 int			ft_isascii(int c);
