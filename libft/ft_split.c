536a364e3578b46e4dfdd7bb12a4f14526fb2bc9
Tyssan <tbrunier@student.42perpignan.fr>
2024-01-15 19:48:18 +0100

    save2split_commentaires

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 void	*ft_memcpysplit(void *dest, const void *src, size_t n)
@@ -24,7 +23,7 @@ void	*ft_memcpysplit(void *dest, const void *src, size_t n)
 	src_p = src;
 	while (++i < (int)n)
 		dest_p[i] = src_p[i];
-	dest_p[i] = '\0';
+	dest_p[i] = '\0'; /*ajout du \0 a la fin de la destination*/
 	return (dest);
 }
 
@@ -39,7 +38,7 @@ size_t	ft_count_words(char const *s, char c)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] != c && (i == 0 || s[i - 1] == c))
+		if (s[i] != c && (i == 0 || s[i - 1] == c)) /*verifie si actuel nest pas le sep et qu'un sep est present avant ou si cétait le debut*/
 			n++;
 		i++;
 	}
@@ -53,7 +52,7 @@ size_t	ft_strlen_sep(const char *s, char c)
 	if (!s)
 		return (0);
 	i = 0;
-	while (s[i] && s[i] != c)
+	while (s[i] && s[i] != c) /*strlen basique mais arrete de compter si un separateur est croisé*/
 		i++;
 	return (i);
 }
@@ -78,13 +77,13 @@ int	ft_splitloop(char const **s, char c, char ***tab, size_t wordnb)
 		if (!(*tab)[i])
 		{
 			while (i != 0)
-				free ((*tab)[--i]);
-			return (free (*tab), 0);
+				free ((*tab)[--i]); /*na pas lair important dapres les tests francinette*/
+			return (free (*tab), 0); 
 		}
-		ft_memcpysplit((*tab)[i++], *s, str_len);
-		*s = *s + str_len;
+		ft_memcpysplit((*tab)[i++], *s, str_len); /*ajoute ce quil y a a partir du ponteur s dans le tableau i du tableau principal*/
+		*s = *s + str_len; /*incremente s par la taille du mot precedent, afin de passer au prochain a la prochaine iteration de la boucle*/
 	}
-	return (1);
+	return (1); /*retourne 1 au lieu de 0 pour pas valider la condition qui verifie si cest un 0, 0 etant le code d'erreur*/
 }
 
 char	**ft_split(char const *s, char c)
@@ -92,15 +91,15 @@ char	**ft_split(char const *s, char c)
 	char	**tab;
 	size_t	wordnb;
 
-	wordnb = ft_count_words(s, c);
 	if (!s)
 		return (NULL);
-	tab = malloc(sizeof(char *) * (wordnb + 1));
+	wordnb = ft_count_words(s, c);
+	tab = malloc(sizeof(char *) * (wordnb + 1)); /*MALLOCage du tableau qui contienra les sous tableaux avec le nombre de mots de s*/
 	if (!tab)
 		return (NULL);
 	if (!ft_splitloop(&s, c, &tab, wordnb))
 		return (tab);
-	tab[wordnb] = NULL;
+	tab[wordnb] = NULL; /*NULLifie le dernier tableau*/
 	return (tab);
 }
 
@@ -117,4 +116,5 @@ char	**ft_split(char const *s, char c)
 	while (res[i] != NULL)
 		printf("%s\n", res[i++]);
 	return (0);
-}*/
+}
+*/
